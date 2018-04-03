21fd4299aac2b6c8d0e074c44b8f4eef01755601
Awadhesh1994 <38037465+Awadhesh1994@users.noreply.github.com>
2018-04-03 16:46:11 +0530

    Update header.js
    changes in header

--- mail_box/src/components/header.js
+++ mail_box/src/components/header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => (
     <nav className="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
         <div className="container">
-            <Link to="/" className="navbar-brand">CashPositive</Link>
+            <Link to="/" className="navbar-brand">Awadhesh MailBox</Link>
             {/* <div className="collapse navbar-collapse justify-content-end pull-right" id="navbarCollapse">
                 <span className="profile-text-new"> Awadhesh Kumar </span>
                 <img className="imge-profile-header" alt='profile' role='presentation' src="../img/ProfilePhoto.jpg"/>
